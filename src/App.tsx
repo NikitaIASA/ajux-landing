ff2a289afc86144de5b3478f1d1dfb7cc1684e98
Nikita Chaika <iasastudent2021@gmail.com>
2023-07-31 12:13:20 +0300

    add: hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Container } from "./components/Container/Container";
-import { Header } from "./components/Header/Header";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Container>
         <Header />
+        <Hero/>
       </Container>
     </>
   );
