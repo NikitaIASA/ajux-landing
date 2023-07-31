c7dbcd092e02ce6cd44749438a507296ccfdd81f
Nikita Chaika <iasastudent2021@gmail.com>
2023-07-31 11:37:51 +0300

    add: header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./components/Container/Container";
+import { Header } from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+    </>
+  );
+}
+
+export default App;
