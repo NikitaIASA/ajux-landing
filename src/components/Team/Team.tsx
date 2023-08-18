fa48df5c078992e88234b3269dbb278d99860b33
Nikita Chaika <iasastudent2021@gmail.com>
2023-08-19 00:50:27 +0300

    fix: containers fix

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -3,14 +3,17 @@ import { FC } from "react";
 import "./Team.scss";
 import TeamList from "../TeamList";
 import SectionHeader from "../SectionHeader";
+import Container from "../Container";
 
 interface TeamProps {}
 
 export const Team: FC<TeamProps> = () => {
   return (
-    <section className="team container">
-      <SectionHeader title="Our Team" subtitle="Team" />
-      <TeamList />
+    <section className="team">
+      <Container>
+        <SectionHeader title="Our Team" subtitle="Team" />
+        <TeamList />
+      </Container>
     </section>
   );
 };
